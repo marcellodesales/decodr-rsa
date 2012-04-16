b917489630526639396bf2e482e59ebd3a865a0f
Marcello de Sales <marcello.desales@gmail.com>
2012-04-16 01:54:44 -0700

    This revision adds initial configuration based on the documentation at http://grails.org/doc/latest/guide/conf.html#config
    * BootStrap.groovy
    - Adding initial identification of the environment for the init and
      destroy methods.
    
    * BuildConfig.groovy
    - Changing the configuration to use Servlet 3.0 API, as well as using
    Java 7 API source level.

--- web/grails-app/conf/BootStrap.groovy
+++ web/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,39 @@
+import javax.servlet.ServletContext;
+import grails.util.Environment
+
 class BootStrap {
 
-    def init = { servletContext ->
+  def init = { ServletContext ctx ->
+
+    switch (Environment.current) {
+
+      case Environment.DEVELOPMENT:
+        ctx.setAttribute("env", "dev")
+        break
+
+      case Environment.PRODUCTION:
+        ctx.setAttribute("env", "prod")
+        break
     }
-    def destroy = {
+    
+    Environment.executeForCurrentEnvironment {
+      production {
+          // do something in production
+      }
+      development {
+          // do something only in development
+      }
     }
+  }
+
+  def destroy = {
+    environments {
+      production {
+        log.debug("Terminating the server...")
+      }
+      development {
+        log.debug("Cleaning resources...")
+      }
+    }
+  }
 }
